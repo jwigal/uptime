34cd34aa8a4d119a810636c2328dbf0581634da1
Jeff Wigal <jeff@wigaldesign.com>
2011-08-26 11:47:07 -0400

    add twitter auth fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110826123839) do
+ActiveRecord::Schema.define(:version => 20110826154353) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -42,6 +42,9 @@ ActiveRecord::Schema.define(:version => 20110826123839) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
