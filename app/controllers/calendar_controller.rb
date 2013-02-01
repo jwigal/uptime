50ffbbbf5392e50e0bd672499622e4af7d417e2c
Jeff Wigal <jeff@assignr.com>
2013-01-31 23:35:04 -0500

    use all-year calendar format

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,10 +1,17 @@
 class CalendarController < ApplicationController
   def index
+    @outages = Outage.where(["date >= ?", 1.year.ago]).all
     respond_to do |format|
-      format.html
-      format.json do
-        @outages = Outage.where(["date >= ?", 1.year.ago])
+      format.html do
+        @dates = (1..12).collect do |i| 
+          (Time.zone.local(Time.zone.now.year, i,1) if Time.zone.local(Time.zone.now.year, i,1).end_of_month  <= Time.zone.now.end_of_month) ||
+          Time.zone.local(Time.zone.now.year-1, i,1)
+        end
+        @uptime = {3 => Outage.uptime(3.months.ago) * 100.0}
+        @uptime[6] = Outage.uptime( 6.months.ago) * 100.0
+        @uptime[12] = Outage.uptime( 12.months.ago) * 100.0
       end
+      format.json
     end
   end
 
---
+++ test/functional/outages_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class OutagesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
