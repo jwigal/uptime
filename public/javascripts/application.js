a5c10a8384ae6538b7f081262e1fab596afa95fc
Jeff Wigal <jeff@assignr.com>
2013-04-22 12:53:57 -0400

    fix problem with pop-up obscured on left side of screen

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function(){
-  $(".red, .yellow").tooltip({ offset: [10, 2],
+  $(".red, .yellow").tooltip({ offset: [10, 50],
       predelay: 200,
       effect: 'slide'  
     });
