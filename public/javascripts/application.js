f2927a154570e20116415cdb69efd86c62c84f0a
Jeff Wigal <jeff@assignr.com>
2013-04-22 12:48:07 -0400

    fix problem with floats and calendar days

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,18 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-$("div.red").tooltip(
-  { offset: [10, 2],
-    predelay: 1000,
-    effect: 'slide'  
-  }      
-  ).dynamic({ bottom: { direction: 'down', bounce: true } });
+$(document).ready(function(){
+  $(".red, .yellow").tooltip({ offset: [10, 2],
+      predelay: 200,
+      effect: 'slide'  
+    });
+    //.dynamic({ bottom: { direction: 'down', bounce: true } });
+	/*
+  $("div.red").tooltip(
+    { offset: [10, 2],
+      predelay: 1000,
+      effect: 'slide'  
+    }      
+    );
+    //.dynamic({ bottom: { direction: 'down', bounce: true } });
+*/  
+});
